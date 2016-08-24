dec5f6901a77fc770d73c6622140dc43b18d7916
Vacko <vacko3@gmail.com>
2016-08-24 21:13:34 +0200

    Added ZoomHack

---
+++ ZoomHack/ZoomHack/Program.cs
@@ -0,0 +1,22 @@
+﻿using DotaSharp;
+
+namespace ZoomHack
+{
+    public class Program
+    {
+        #region Static Methods
+
+        public static void Main()
+        {
+            Game.OnGameStart += OnGameStart;
+        }
+
+        public static void OnGameStart()
+        {
+            Console.FindVar("dota_camera_distance").RemoveFlags(ConVarFlags.FCVAR_CHEAT).SetValue(1500);
+            Console.FindVar("r_farz").SetValue(18000);
+        }
+
+        #endregion
+    }
+}
