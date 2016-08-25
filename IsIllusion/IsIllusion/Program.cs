675209ff556fc405de07318a01a7a49ba3e161c9
Vacko <vacko3@gmail.com>
2016-08-25 21:02:01 +0200

    Added IsIllusion

---
+++ IsIllusion/IsIllusion/Program.cs
@@ -0,0 +1,30 @@
+﻿using DotaSharp;
+using System.Linq;
+
+namespace IsIllusion
+{
+    internal class Program
+    {
+        #region Static Methods
+
+        public static void Main()
+        {
+            Drawing.OnDraw += DrawIllusion;
+        }
+
+        public static void DrawIllusion()
+        {
+            var enemyPlayers = ObjectMgr.GetEntities<Hero>().Where(x => x.IsEnemy()).ToList();
+
+            foreach (var player in enemyPlayers)
+                if (player.IsIllusion())
+                {
+                    Vector2 vScreen;
+                    if (Drawing.WorldToScreen(player.NetworkOrigin, out vScreen))
+                        Drawing.DrawText(vScreen.X, vScreen.Y, "Illusion", new Color(255, 255, 255));
+                }
+        }
+
+        #endregion
+    }
+}
