40e5fa93a454081a8379a08d36b9cf224842271d
Vacko <vacko3@gmail.com>
2016-08-26 20:57:21 +0200

    Added Weather

---
+++ Weather/Weather/Program.cs
@@ -0,0 +1,39 @@
+﻿using DotaSharp;
+
+namespace Weather
+{
+    public class Program
+    {
+        #region Static Methods
+
+        public static void Main()
+        {
+            Game.OnGameStart += OnGameStart;
+        }
+
+        public static void OnGameStart()
+        {
+            Console.FindVar("cl_weather").RemoveFlags(ConVarFlags.FCVAR_CHEAT).SetValue((int) WeatherType.Ash);
+        }
+
+        #endregion
+
+        #region Nested Types, Enums, Delegates
+
+        private enum WeatherType
+        {
+            Default = 0,
+            Snow = 1,
+            Rain = 2,
+            Moonbeam = 3,
+            Pestilence = 4,
+            Harvest = 5,
+            Sirocco = 6,
+            Spring = 7,
+            Ash = 8,
+            Aurora = 9
+        }
+
+        #endregion
+    }
+}
